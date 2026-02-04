7d9371a492d34be1cc253c1ce5ce898407d8c8d8
The Great White Rabbit <gribomen@mail.ru>
2026-02-04 22:33:35 +0300

    Изучил интернационализацию чисел

--- app.js
+++ app.js
@@ -1,48 +1,30 @@
-'use strict'
+'use strict';
 
-//Изучаю блок Расчеты и математика
+const optionsl = {
+    style: 'currency',
+    currency: "RUB"
+}
 
-/*Первый урок */
-// console.log(10 === 10.0);
-// //0.1
-// console.log(0.1+0.2 === 0.3);
-// console.log(0.1+0.2);
+const options2 = {
+    style: 'currency',
+    currency: 'USD'
+}
 
-// console.log(Number('10'));
-// console.log(+'20');
-// console.log(Number.parseInt('11',10));
-// console.log(Number.parseInt('11 sec',10));
-// console.log(Number.parseInt('sec 11',10));
+const options3 = {
+    style: 'decimal',
+}
 
-// console.log(Number.parseFloat('11.5',10));
-// console.log(Number.parseFloat('11.5 sec',10));
-// console.log(Number.parseFloat('sec 11.5',10));
+const options4 = {
+    style: 'percent'
+}
 
-// console.log(Number.isNaN(Number('10sdfs')));
-// console.log(Number.isNaN(10/0));
-// console.log(Number.isFinite(10/0));
-// console.log(Number.isFinite(Number('10sdfs')));
-// console.log(Number.isFinite(10));
+const options5 = {
+    style: 'unit',
+    unit: 'celsius'
+}
 
-/*Второй урок  */
-console.log(Math.sqrt(36));
-console.log(36 ** (1/2));
-console.log(Math.cbrt(27));
-console.log(16 ** (1/4));
-
-console.log(Math.sign(-100));
-console.log(Math.sign(35));
-console.log(Math.abs(-100));
-console.log(Math.abs(35));
-
-console.log(Math.exp(3));
-
-console.log(Math.max(1, -2, 10, 0, 19));
-console.log(Math.max(1, -2, '10', true, '19'));
-console.log(Math.max(1, -2, '10', true, 1.2));
-console.log(Math.min(1, -2, 10, 0, 19));
-
-const arr = [1, -2, 10, 0, 19];
-console.log(Math.max(...arr));
-
-console.log(Math.random());
+console.log(new Intl.NumberFormat('ru-RU',optionsl).format(23000));
+console.log(new Intl.NumberFormat('ru-RU',options2).format(23000));
+console.log(new Intl.NumberFormat('ru-RU',options3).format(10000));
+console.log(new Intl.NumberFormat('ru-RU',options4).format(0.1));
+console.log(new Intl.NumberFormat('ru-RU',options5).format(23));
