c1b2bd33d72066eea8af93e5cfd814e526921840
The Great White Rabbit <gribomen@mail.ru>
2026-02-18 16:14:27 +0300

    Изучил интернационализацию дат

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 'use strict';
 
-const first = new Date(2024, 10, 15);
-const second = new Date(2024, 10, 15);
+const date = new Date();
 
-console.log(first < second);
-console.log(first == second);
-console.log(first === second);
+console.log(date);
+console.log(new Intl.DateTimeFormat('ru-RU').format(date));
+const options1 = {
+    hour: 'numeric',
+    minute:'numeric'
+};
+console.log(new Intl.DateTimeFormat('ru-RU', options1).format(date));
+const options2 = {
+    hour:'numeric',
+    minute:'numeric',
+    month:'long',
+    weekday: 'short',
+    year:'2-digit'
+};
+console.log(new Intl.DateTimeFormat('en-US', options2).format(date));
+console.log(new Intl.DateTimeFormat('en-US', options2).format(date));
 
-console.log(first.getTime() == second.getTime());
-console.log(first.getTime() === second.getTime());
-console.log(Number(first) === Number(second));
-console.log(+first === +second);
+console.log(navigator.language);
+console.log(new Intl.DateTimeFormat(navigator.language,options1).format(date));
+
+ 
+
+
+ 
+
+
+
+
+
+
+
+
