86cd2efaf77a2aaeab6f46612f5c22f426b1c7af
The Great White Rabbit <gribomen@mail.ru>
2026-02-20 08:45:34 +0300

    Изучил самое первое создание объектов через функции-конструктор

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 'use strict';
 
-const interval = setInterval(() => {
-    console.log(new Date());
-}, 1000);
+const User = function (email, password){
+    this.email = email;
+    this.password = password;
+};
 
-const timer = setTimeout(() => {
-    clearInterval(interval);
-    console.log(new Date());
-}, 5000);
+const user1 = new User('ader@lu.ru','123');
+console.log(user1);
+const user2 = new User('ader2@lu.ru','123');
+console.log(user2)
+console.log(user2 instanceof User);
 
-console.log(interval);
-console.log(timer);
+// Создается пустой объект
+// Вызывается User функция
+// this = пустому объекту
+// объект связывается с prototype
+// возвращается объект
