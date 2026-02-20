62ed0af881dd426f8cf77e394c64ec2317c93ebf
The Great White Rabbit <gribomen@mail.ru>
2026-02-20 10:58:23 +0300

    Реализовал мехонизм добавления товара в корзину

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
-'use strict'
+'use strict';
 
-const timeCooking = "01.30";
+const product = { id: 1, name: 'Bread', count: 1};
+const product2 = { id: 2, name: 'Knife', count: 3};
 
-function getTimerCookPizza(ms){
-    const end = new Date().getTime() + ms; 
-    const interval = setInterval(() =>{
-        console.log(
-            new Intl.DateTimeFormat(navigator.language,{
-                minute:"numeric",
-                second:"numeric"
-            }).format(end + 100 - new Date())
-        );
-    },1000);
-
-    setTimeout(()=>{
-        clearInterval(interval);
-        console.log("🍕!!!");
-    },ms);
-
+const Product = function({id,name,count}){
+    this.id = id;
+    this.name = name;
+    this.count = count;
 }
 
-getTimerCookPizza(35000);
+const Basket =  function(){
+    this.products = [];
+}
+
+Basket.prototype.addProduct = function(product){
+    if(this.products.find(item => item.id == product.id)){
+        return;
+    }
+    this.products.push(product);
+}
+
+Basket.prototype.increaseProduct = function (product){
+    const currentProduct =  this.products.find( 
+        item=> item.id == product.id);
+    currentProduct.count++;
+}
+
+Basket.prototype.reduceProduct = function (product){
+    const currentProduct =  this.products.find( 
+        item=> item.id == product.id);
+    if(currentProduct.count - 1 == 0){
+        this.product.splice(this.products.findIndex(item => item.id == product.id),1);        
+    }else{
+        currentProduct.count--;
+    }
+}
+
+const basket =  new Basket();
+basket.addProduct(product);
+basket.addProduct(product);
+basket.addProduct(product2);
+
+basket.increaseProduct(product);
+basket.increaseProduct(product);
+basket.increaseProduct(product);
+basket.increaseProduct(product);
+
+basket.increaseProduct(product2);
+basket.increaseProduct(product2);
+
+basket.reduceProduct(product);
+
+basket.reduceProduct(product2);
+basket.reduceProduct(product2);
+basket.reduceProduct(product2);
+basket.reduceProduct(product2);
+console.log(basket);
