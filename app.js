37b85b475228545f6e87a033813dbbe846bfac5f
The Great White Rabbit <gribomen@mail.ru>
2026-02-20 15:30:18 +0300

    Сделал домашнююю работу по прототипам сложно

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 'use strict';
 
-const Book = function(title, author){
-    this.author = author;
-    this.title = title
-    this.isRead = false;
+function Hero (param){
+    this.species = param.species;
+    this.name = param.name;
+    this.language = param.language;
 }
 
-Book.prototype.read = function() {
-    this.isRead = true;
+Hero.prototype.talk= function(){
+    console.log(`Я ${this.name} и говорю на ${this.language}`);
 }
 
-const lordOftheRing = new Book ('Load of the ring', 'Tolkien');
-lordOftheRing.read();
 
-console.log(lordOftheRing);
+const Orc = function (paramHero, weapon){
+    Hero.call(this,paramHero);
+    this.weapon = weapon;
+};
 
-console.log(lordOftheRing.__proto__);
-console.log(lordOftheRing.__proto__ === Book.prototype);
-console.log(Book.prototype.isPrototypeOf(lordOftheRing));
-console.log(Book.prototype.isPrototypeOf(Book));
+Orc.prototype.talk = Hero.prototype.talk;
+
+Orc.prototype.hit = function(){
+    console.log(` Я бью ${this.weapon}`); 
+}  
+
+const Elf = function (paramHero,spell){
+    Hero.call(this,paramHero);
+    this.spell = spell;
+};
+
+Elf.prototype.castSpell= function (){
+    console.log(`Кастую заклинание ${this.spell}`);
+}
+
+const orc = new Orc({species: "орк", name: "Болг", language:"Гундабадский"},"топор");
+const elf = new Elf({species: "эльф", name: "Леголайз", language:"Валарский"},"огненный шар")
+console.log(orc);
+console.log(elf);
+elf.castSpell();
