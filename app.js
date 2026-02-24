f41cd83570bc5ec9c009ad145fef6df1c4085608
The Great White Rabbit <gribomen@mail.ru>
2026-02-24 17:03:30 +0300

    Решил практику по созданию класса user с условиями

--- app.js
+++ app.js
@@ -1,58 +1,47 @@
 'use strict';
 
-const product = { id: 1, name: 'Bread', count: 1};
-const product2 = { id: 2, name: 'Knife', count: 3};
+class User {
+    #login;
+    #_password;
 
-const Product = function({id,name,count}){
-    this.id = id;
-    this.name = name;
-    this.count = count;
-}
-
-const Basket =  function(){
-    this.products = [];
-}
-
-Basket.prototype.addProduct = function(product){
-    if(this.products.find(item => item.id == product.id)){
-        return;
+    #getPassword(){
+        return this.#password;
     }
-    this.products.push(product);
-}
 
-Basket.prototype.increaseProduct = function (product){
-    const currentProduct =  this.products.find( 
-        item=> item.id == product.id);
-    currentProduct.count++;
-}
+    constructor(login, password){
+        this.#login = login;
+        this.#password = password;
+    }
 
-Basket.prototype.reduceProduct = function (product){
-    const currentProduct =  this.products.find( 
-        item=> item.id == product.id);
-    if(currentProduct.count - 1 == 0){
-        this.product.splice(this.products.findIndex(item => item.id == product.id),1);        
-    }else{
-        currentProduct.count--;
+    set #password(pass){
+        this.#_password = pass.split('').reverse().join('');
+    }
+
+    get #password(){
+        return this.#_password.split('').reverse().join('');
+    }
+
+    get login(){
+        return this.#login;
+    }
+
+    verificationPassword(password){
+        return this.#password == password;
+    }
+
+    changePassword(lastPassword, newPassword){
+        if(this.verificationPassword(lastPassword)){
+            this.#password = newPassword;
+            return true;
+        }
+        return false;
     }
 }
 
-const basket =  new Basket();
-basket.addProduct(product);
-basket.addProduct(product);
-basket.addProduct(product2);
+const user1 = new User( "slava","123");
 
-basket.increaseProduct(product);
-basket.increaseProduct(product);
-basket.increaseProduct(product);
-basket.increaseProduct(product);
-
-basket.increaseProduct(product2);
-basket.increaseProduct(product2);
-
-basket.reduceProduct(product);
-
-basket.reduceProduct(product2);
-basket.reduceProduct(product2);
-basket.reduceProduct(product2);
-basket.reduceProduct(product2);
-console.log(basket);
+console.log(user1.verificationPassword("123"));
+console.log(user1.verificationPassword("137"));
+user1.changePassword("123","137");
+console.log(user1.verificationPassword("123"));
+console.log(user1.verificationPassword("137"));
