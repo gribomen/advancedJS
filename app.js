c83ae52d9fd0e830a35ddf3026f76335bf36554f
The Great White Rabbit <gribomen@mail.ru>
2026-02-24 21:12:13 +0300

    Посмотрел как работает наследованеи под копотом и в es6 классах

--- app.js
+++ app.js
@@ -1,27 +1,57 @@
 'use strict';
 
-const Book = function(title, author){
-    this.title =  title;
-    this.author = author;
+// const Book = function(title, author){
+//     this.title =  title;
+//     this.author = author;
+// }
+
+
+// Book.prototype.buy = function(){
+//     console.log('Buy');
+// }
+
+// const AudioBook = function(title, author, lenMin){
+//     Book.call(this,title, author);
+//     this.lenMin = lenMin;
+// }
+
+// AudioBook.prototype = Object.create(Book.prototype);
+// AudioBook.prototype.constructor = AudioBook;
+// AudioBook.prototype.log = function(){
+//     console.log(`${this.title} -  ${this.lenMin}`);
+// }
+
+// const book =  new AudioBook('Lord Of The Rings', 'Tolkien', 20*60);
+// book.log();
+// book.buy();
+// console.log(book);
+
+
+class Book{
+    
+    constructor(title, author){
+        this.title =  title;
+        this.author = author;
+    }
+
+    buy(){
+        console.log('Buy');
+    }
 }
 
+class AudioBook extends Book {
 
-Book.prototype.buy = function(){
-    console.log('Buy');
+    constructor(title, author, lenMin){
+        super(title,author);
+        this.lenMin = lenMin;
+    }
+
+    log() {
+        console.log(`${this.title} -  ${this.lenMin}`);
+    }
+
 }
 
-const AudioBook = function(title, author, lenMin){
-    Book.call(this,title, author);
-    this.lenMin = lenMin;
-}
-
-AudioBook.prototype = Object.create(Book.prototype);
-AudioBook.prototype.constructor = AudioBook;
-AudioBook.prototype.log = function(){
-    console.log(`${this.title} -  ${this.lenMin}`);
-}
-
-const book =  new AudioBook('Lord Of The Rings', 'Tolkien', 20*60);
+const book = new AudioBook('Lord Of The Rings', 'Tolkien', 20*60);
 book.log();
 book.buy();
-console.log(book);
