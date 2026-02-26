b4f8efe9ef3d6f4d09239c5a0bec8262354d1cd2
The Great White Rabbit <gribomen@mail.ru>
2026-02-26 14:35:25 +0300

    Изучил патерн Builder и  chaining

--- app.js
+++ app.js
@@ -1,67 +1,44 @@
 'use strict';
 
-'use strict';
+class Wallet{
+    balance = 0;
 
-class Enemy {
-    health;
-    constructor(health){
-        this.health = health;
+    add(sum){
+        this.balance +=sum;
+        return this;
     }
 
-    getDemage(damage){
-        if(damage < this.health){
-            this.health = this.health - damage;
-            console.log(this.health);
-            return;
-        }
-
-        this.health = 0;
+    remove (sum){        
+        this.balance -=sum;
+        return this;
     }
 }
 
-class Sword {
-    #damage;
-    constructor(damage){
-        this.#damage = damage;
+const wallet = new Wallet();
+const res = wallet
+    .add(100)
+    .remove(10)
+    .add(15)    
+;
+console.log(res);
+
+class Builder{
+    house = [];
+
+    addRoof() {
+        this.house.roof = 'Roof';
+        return this;
     }
 
-    strike(enemy){
-        enemy.getDemage(this.#damage);
-    }
-}
-
-class Orc extends Enemy{
-    constructor(health){
-        super(health);
+    addFloor() {
+        this.house.floor = 'Floor';
+        return this;
     }
 
-    getDemage(damage){
-        if(Math.random() > 0.5){
-            if(this.health> damage){
-                this.health = this.health - damage;
-            }else{
-                this.health = 0;
-            }
-            console.log(this.health);
-            return;
-        }
-        console.log(this.health);
+    execute() {
+        return this.house;
     }
 }
 
-class Troll extends Enemy{
-
-}
-
-const enemy1 = new Orc(10);
-const sword1 = new Sword(3);
-const troll1 = new Troll(20);
-sword1.strike(enemy1);
-sword1.strike(enemy1);
-sword1.strike(enemy1);
-sword1.strike(troll1);
-sword1.strike(troll1);
-sword1.strike(troll1);
-// ad-hock полиморфизм
-'2' + '4'
-2 + 4
+const house = new Builder().addRoof().addFloor().execute();
+console.log(house);
