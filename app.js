973db82e60df9dd169e14cf7e47e29423af213b4
The Great White Rabbit <gribomen@mail.ru>
2026-02-26 20:37:50 +0300

    Изучил принцип открытости и закрытости

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 'use strict';
 
-class Character{
-    #inventory = [];
-    #health = 10;
-    
-    pickItem(item){
-        this.#inventory.push(item);
-    }
-
-    recieveDamage(damage){
-        this.#health -= damage;
-    }
-
+class Treasure {
+    value = 0;
 }
 
-class DB {
-    saveCharacter(){
-        localStorage.setItem('char',this);
-    }
+class Coin  extends Treasure{
+    value = 1;
+}
 
-    loadCharacter(){
-        localStorage.setItem('char',this);
+class Crystal extends Treasure{
+    value = 10;
+}
+
+class Crystal extends Treasure{
+    value = 20;
+}
+
+class Inventory{
+    #score;
+    pick(treasure){
+        this.#score += treasure.value;
     }
 }
