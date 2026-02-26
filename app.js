ec19f71ca05e2aae7ade62cd02940185aebe83ef
The Great White Rabbit <gribomen@mail.ru>
2026-02-26 17:12:16 +0300

    Изучил принцип единой ответственности

--- app.js
+++ app.js
@@ -1,44 +1,25 @@
 'use strict';
 
-class Wallet{
-    balance = 0;
-
-    add(sum){
-        this.balance +=sum;
-        return this;
+class Character{
+    #inventory = [];
+    #health = 10;
+    
+    pickItem(item){
+        this.#inventory.push(item);
     }
 
-    remove (sum){        
-        this.balance -=sum;
-        return this;
+    recieveDamage(damage){
+        this.#health -= damage;
     }
+
 }
 
-const wallet = new Wallet();
-const res = wallet
-    .add(100)
-    .remove(10)
-    .add(15)    
-;
-console.log(res);
-
-class Builder{
-    house = [];
-
-    addRoof() {
-        this.house.roof = 'Roof';
-        return this;
+class DB {
+    saveCharacter(){
+        localStorage.setItem('char',this);
     }
 
-    addFloor() {
-        this.house.floor = 'Floor';
-        return this;
-    }
-
-    execute() {
-        return this.house;
+    loadCharacter(){
+        localStorage.setItem('char',this);
     }
 }
-
-const house = new Builder().addRoof().addFloor().execute();
-console.log(house);
