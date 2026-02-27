27c826175fb79b465a4d70d5e678d141d7e8e555
The Great White Rabbit <gribomen@mail.ru>
2026-02-27 15:29:57 +0300

    Сделал практику по Расчет цены продукта

--- app.js
+++ app.js
@@ -1,62 +1,14 @@
 'use strict';
 
-class Enemy {
-    health;
-    constructor(health){
-        this.health = health;
-    }
+let medianPrice = 0;
+const request = new XMLHttpRequest();
+request.open('GET','https://dummyjson.com/products/');
+request.send();
 
-    getDemage(damage){
-        if(damage < this.health){
-            this.health = this.health - damage;
-            console.log(this.health);
-            return;
-        }
-
-        this.health = 0;
-    }
-}
-
-class Sword {
-    #damage;
-    constructor(damage){
-        this.#damage = damage;
-    }
-
-    strike(enemy){
-        enemy.getDemage(this.#damage);
-    }
-}
-
-class Orc extends Enemy{
-    constructor(health){
-        super(health);
-    }
-
-    getDemage(damage){
-        if(Math.random() > 0.5){
-            if(this.health> damage){
-                this.health = this.health - damage;
-            }else{
-                this.health = 0;
-            }
-            console.log(this.health);
-            return;
-        }
-        console.log(this.health);
-    }
-}
-
-const enemy1 = new Orc(10);
-const sword = new Sword(6)
-sword.strike(enemy1);
-sword.strike(enemy1);
-sword.strike(enemy1);
-sword.strike(enemy1);
-sword.strike(enemy1);
-sword.strike(enemy1);
-sword.strike(enemy1);
-sword.strike(enemy1);
-sword.strike(enemy1);
-sword.strike(enemy1);
-sword.strike(enemy1);
+request.addEventListener('load',function(){
+    const data = JSON.parse(this.responseText).products;
+    data.forEach(element => {
+        medianPrice += (element.price/data.length);
+    });
+    console.log(Math.round(medianPrice));
+});
