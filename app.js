3ae6c088fd2aa81bc2820ae547d41f0786f794d7
The Great White Rabbit <gribomen@mail.ru>
2026-03-01 02:20:51 +0300

    Познакомился как обработать цепочку промиссов

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 'use strict';
 
-fetch('https://dummyjson.com/products')
-    .then((response) => response.json())
-    .then(({products})=>{
+// 1 Способ обработать ошибку в json (Способ говно)
+fetch('https://dummyjson.com/productss')
+    .then(
+        response => {
+            console.log(response);
+            return response.json();
+        },
+        error => console.log(error)
+    )
+    .then(({products}) => {
+        console.log(products);
         return fetch('https://dummyjson.com/products/' + products[0].id);
-    })
+    },
+    error => console.log(error))
     .then( response => response.json())
     .then( data => {
         console.log(data);
     });
+
+// 2 Способ обработать ошибку в json
+fetch('https://dummyjson.com/productss')
+    .then(
+        response => {
+            console.log(response);
+            return response.json();
+        },
+        error => console.log(error)
+    )
+    .then(({products}) => {
+        console.log(products);
+        return fetch('https://dummyjson.com/products/' + products[0].id);
+    },
+    error => console.log(error))
+    .then( response => response.json())
+    .then( data => {
+        console.log(data);
+    }).catch(error => console.log(error));
