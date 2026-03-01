2fd5285e9525c7d5c74e409fc75140de590fcec1
The Great White Rabbit <gribomen@mail.ru>
2026-03-01 14:11:18 +0300

    Изучил последний метод для обработки запросов finally

--- app.js
+++ app.js
@@ -1,39 +1,22 @@
 'use strict';
 
-// 1 Способ обработать ошибку в json (Способ говно)
 fetch('https://dummyjson.com/productss')
     .then(
         response => {
             console.log(response);
             return response.json();
-        },
-        error => console.log(error)
+        }
     )
     .then(({products}) => {
-        console.log(products);
-        return fetch('https://dummyjson.com/products/' + products[0].id);
-    },
-    error => console.log(error))
+            console.log(products);
+            return fetch('https://dummyjson.com/products/' + products[0].id);
+        }
+    )
     .then( response => response.json())
     .then( data => {
         console.log(data);
+    })
+    .catch(error => console.log(error))
+    .finally(() =>{
+        console.log('Finally');
     });
-
-// 2 Способ обработать ошибку в json
-fetch('https://dummyjson.com/productss')
-    .then(
-        response => {
-            console.log(response);
-            return response.json();
-        },
-        error => console.log(error)
-    )
-    .then(({products}) => {
-        console.log(products);
-        return fetch('https://dummyjson.com/products/' + products[0].id);
-    },
-    error => console.log(error))
-    .then( response => response.json())
-    .then( data => {
-        console.log(data);
-    }).catch(error => console.log(error));
