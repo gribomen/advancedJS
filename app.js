a2b173c2ac826ae6a0b891f88cdeeefa1a2f93ba
The Great White Rabbit <gribomen@mail.ru>
2026-03-02 21:19:52 +0300

    Изучил функцию withResolvers() для Promise

--- app.js
+++ app.js
@@ -1,12 +1,64 @@
 'use strict';
 
+// function wait(msec) {
+//     const {resolve,reject,promise} = Promise.withResolvers();
+//     setTimeout(() => {
+//         resolve();
+//     },msec);
+//     return promise;
+//     // 1 Вариант
+//     // return new Promise((resolve,reject)=>{
+//     //     setTimeout(()=>{
+//     //         resolve();
+//     //     },msec)
+//     // })
+// }
 
-const prom = new Promise((resolve) => {
-    console.log('Constructor');
-    setTimeout(() => {
-        resolve('Timer');
-    },1000);
-});
-prom.then(data => console.log(data));
-Promise.reject(new Error('Error')).catch(error => console.error(error));
-Promise.resolve('Instant').then(data => console.log(data));
+
+// async function run() {
+//     console.log('начало');
+//     await wait(2000);
+//     console.log('конец');
+// }
+
+// run();
+
+
+class Queue {
+    #message = [];
+    #resolve;
+    #reject;
+    #promise;
+
+    constructor(){
+        const {resolve,reject,promise} = Promise.withResolvers();
+        this.#resolve = resolve;
+        this.#reject = reject;
+        this.#promise = promise;
+    };
+
+    add(msg){
+        this.#message.push(msg);
+        return this;
+    }
+
+    close(){
+        this.#resolve(this.#message)
+    }
+
+    error(reason){
+        this.#reject(reason);
+    }
+
+    subsribe(){
+        return this.#promise;
+    }
+}
+
+
+const queue = new Queue();
+const sub1 = queue.subsribe();
+sub1.then(data => console.log(data)).catch(error => console.error(error));
+const sub2 = queue.subsribe();
+sub2.then(data => console.log(data)).catch(error => console.error(error));
+queue.add('msg1').add('msg2').close();
