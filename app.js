eb4a1aae0fcebfabac0e1f92c6a1c99bf03362d4
The Great White Rabbit <gribomen@mail.ru>
2026-03-02 23:17:52 +0300

    Изучил методы async/await

--- app.js
+++ app.js
@@ -1,64 +1,14 @@
 'use strict';
 
-// function wait(msec) {
-//     const {resolve,reject,promise} = Promise.withResolvers();
-//     setTimeout(() => {
-//         resolve();
-//     },msec);
-//     return promise;
-//     // 1 Вариант
-//     // return new Promise((resolve,reject)=>{
-//     //     setTimeout(()=>{
-//     //         resolve();
-//     //     },msec)
-//     // })
-// }
+async function getProducts(){
+    const productsResponse = await fetch('https://dummyjson/products');
+    const {products} = await productsResponse.json();
+    console.log(products);
 
-
-// async function run() {
-//     console.log('начало');
-//     await wait(2000);
-//     console.log('конец');
-// }
-
-// run();
-
-
-class Queue {
-    #message = [];
-    #resolve;
-    #reject;
-    #promise;
-
-    constructor(){
-        const {resolve,reject,promise} = Promise.withResolvers();
-        this.#resolve = resolve;
-        this.#reject = reject;
-        this.#promise = promise;
-    };
-
-    add(msg){
-        this.#message.push(msg);
-        return this;
-    }
-
-    close(){
-        this.#resolve(this.#message)
-    }
-
-    error(reason){
-        this.#reject(reason);
-    }
-
-    subsribe(){
-        return this.#promise;
-    }
+    const productResponse = await fetch('https://dummyjson/products/' + products[0].id );
+    const product = await productResponse.json();
+    console.log(product);
 }
 
-
-const queue = new Queue();
-const sub1 = queue.subsribe();
-sub1.then(data => console.log(data)).catch(error => console.error(error));
-const sub2 = queue.subsribe();
-sub2.then(data => console.log(data)).catch(error => console.error(error));
-queue.add('msg1').add('msg2').close();
+getProducts();
+console.log('End');
