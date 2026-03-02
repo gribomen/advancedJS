ac518a0cf4c857514274fc518a8ef5bb48502b21
The Great White Rabbit <gribomen@mail.ru>
2026-03-02 15:52:37 +0300

    Посмотрел статические методы Promise

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
 'use strict';
 
-const prom = new Promise((resolve,reject)=>{
-    if( new Date() < new Date('07/04/2026')){
-        reject( new Error('Error'));
-    }
-    resolve('Success');
+
+const prom = new Promise((resolve) => {
+    console.log('Constructor');
+    setTimeout(() => {
+        resolve('Timer');
+    },1000);
 });
-
-prom
-    .then(data =>console.log(data))
-    .catch(error => console.log(error))
-
-function timeOut(sec){
-    return new Promise((resolve)=>{
-        setTimeout(()=>{
-            resolve();
-        },1000*sec);
-    })
-}
-
-timeOut(1)
-    .then(() => {
-        console.log(1);
-        return timeOut(1);
-    })    
-    .then(() => {
-        console.log(1);
-    })
-    .then(() => {
-        console.log(1);
-        return timeOut(1);
-    })    
-    .then(() => {
-        console.log(1);
-    });
+prom.then(data => console.log(data));
+Promise.reject(new Error('Error')).catch(error => console.error(error));
+Promise.resolve('Instant').then(data => console.log(data));
