fa744a1911e558def4318f95598204ae8debcf2b
The Great White Rabbit <gribomen@mail.ru>
2026-03-02 21:52:47 +0300

    Сделал практику по созданию своего метода fetch запроса

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 'use strict';
 
-function getData(url, errorMessage){
-    return fetch(url)
-    .then( response => {
-        if(!response.ok){
-            throw new Error(`${errorMessage} ${response.status}`);
-        }
-        return response.json();
-    })
-}
+// 1 Вариант реализации
+// function myFetch(url){
+//     return new Promise((resolve,reject)=>{
+//     const request = new XMLHttpRequest();
+//         request.open('GET',url);
+//         request.send();
+//         request.addEventListener('load',function(){
+//             try{
+//                 resolve(this.response);
+//             }catch(error){
+//                 reject(new Error('error'));
+//             }
+//         })
+//     });
+// }
 
-getData('https://dummyjson.com/products','Can not get products')
-    .then(({products}) => {
-            console.log(products);
-            return getData('https://dummyjson.com/products/' + products[0].id,'Can not get product 1');
-        }
-    )
-    .then( data => {
+// 2 Вариант специализации
+function myFetch(url){
+    return new Promise((resolve,reject)=>{
+    const request = new XMLHttpRequest();
+        request.open('GET',url);
+        request.send();
+        request.addEventListener('load',function(){
+            if(this.status > 400){
+                reject(new Error(this.status));
+            }
+            resolve(this.responseText);
+
+        })
+
+        request.addEventListener('error',function(){
+            reject(new Error(this.status));
+        })
+
+        request.addEventListener('timeout',function(){
+            reject(new Error('Timeout'));
+
+        })
+    });
+}
+myFetch('https://dummysjson.com/products/')
+    .then(data => {
         console.log(data);
     })
-    .catch(error =>{
-        const el = document.querySelector('.filter');
-        el.innerHTML = error;
-    })
+    .catch(error => console.error('Это ошибка: ' + error));
