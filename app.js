d28c7654303334bfbde51529ce4c34fb094f585d
The Great White Rabbit <gribomen@mail.ru>
2026-03-02 22:46:56 +0300

    Сделал домашку по промиссам, да я крут

--- app.js
+++ app.js
@@ -1,47 +1,13 @@
 'use strict';
 
-// 1 Вариант реализации
-// function myFetch(url){
-//     return new Promise((resolve,reject)=>{
-//     const request = new XMLHttpRequest();
-//         request.open('GET',url);
-//         request.send();
-//         request.addEventListener('load',function(){
-//             try{
-//                 resolve(this.response);
-//             }catch(error){
-//                 reject(new Error('error'));
-//             }
-//         })
-//     });
-// }
-
-// 2 Вариант специализации
-function myFetch(url){
-    return new Promise((resolve,reject)=>{
-    const request = new XMLHttpRequest();
-        request.open('GET',url);
-        request.send();
-        request.addEventListener('load',function(){
-            if(this.status > 400){
-                reject(new Error(this.status));
-            }
-            resolve(this.responseText);
-
+function getPosition(){
+    return new Promise((resolve) =>{
+        navigator.geolocation.getCurrentPosition((position) => {
+            resolve(position.coords.latitude + " " + position.coords.longitude);
         })
-
-        request.addEventListener('error',function(){
-            reject(new Error(this.status));
-        })
-
-        request.addEventListener('timeout',function(){
-            reject(new Error('Timeout'));
-
-        })
-    });
-}
-myFetch('https://dummysjson.com/products/')
-    .then(data => {
-        console.log(data);
     })
-    .catch(error => console.error('Это ошибка: ' + error));
+
+}
+
+getPosition()
+    .then(data => console.log(data));
