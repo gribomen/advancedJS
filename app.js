ec251963252fe551f6b2a6a0c985a026503a3ee4
The Great White Rabbit <gribomen@mail.ru>
2026-03-02 22:55:30 +0300

    Добавил обработку ошибки геопозиции

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 'use strict';
 
 function getPosition(){
-    return new Promise((resolve) =>{
+    return new Promise((resolve,reject) =>{
         navigator.geolocation.getCurrentPosition((position) => {
             resolve(position.coords.latitude + " " + position.coords.longitude);
+        },(error) => {
+            reject(error);
         })
     })
 
 }
 
 getPosition()
-    .then(data => console.log(data));
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
