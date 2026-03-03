d35c152d7a8ad3b575721666fea3f00622f3b95c
The Great White Rabbit <gribomen@mail.ru>
2026-03-03 20:44:09 +0300

    Изучил еще стат методы Promise

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 'use strict';
 
-async function getAllProducts(params) {
-    const response = await fetch('https://dummyjson.com/products');
-    return response.json();
-}
-
 async function getProduct(id) {
     const response = await fetch('https://dummyjson.com/products/' + id);
     return response.json();
@@ -17,14 +12,26 @@ async function getProductError(id) {
 }
 
 async function main() {
-    const {products} = await getAllProducts();
-    const res = await Promise.all(products.map(product => getProduct(product.id)));
-    console.log(res);
-    // for(const product of products){
-    //     console.log(product.id);
-    //     const res = await getProduct(product.id);
-    //     console.log(res);
-    // }
+    const res1 = await Promise.all([
+        getProduct(1),
+        getProduct(2),
+        getProduct(3)
+    ]);
+    console.log(res1);
+
+    const res2 = await Promise.allSettled([
+        getProduct(1),
+        getProduct(2),
+        getProductError(3)
+    ]);
+    console.log(res2);
+
+    const res3 = await Promise.race([
+        getProduct(1),
+        getProduct(2),
+        getProduct(3)
+    ]);
+    console.log(res3);
 }
 
 main()
