e5c091a054af8a160ba62823bb2870ca52c029cb
The Great White Rabbit <gribomen@mail.ru>
2026-03-03 21:47:01 +0300

    Изучил другие компоненты метода fetch

--- app.js
+++ app.js
@@ -1,37 +1,21 @@
 'use strict';
 
-async function getProduct(id) {
-    const response = await fetch('https://dummyjson.com/products/' + id);
-    return response.json();
-}
-
-
-async function getProductError(id) {
-    const response = await fetch('https://dummyjsons.com/products/' + id);
-    return response.json();
-}
-
 async function main() {
-    const res1 = await Promise.all([
-        getProduct(1),
-        getProduct(2),
-        getProduct(3)
-    ]);
-    console.log(res1);
-
-    const res2 = await Promise.allSettled([
-        getProduct(1),
-        getProduct(2),
-        getProductError(3)
-    ]);
-    console.log(res2);
-
-    const res3 = await Promise.race([
-        getProduct(1),
-        getProduct(2),
-        getProduct(3)
-    ]);
-    console.log(res3);
+    const resUsers = await fetch('https://dummyjson.com/users');
+    const users = await resUsers.json();
+    console.log(users);
+    const res = await fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: "jamesd",
+            password: "jamesdpass"
+        })
+    });
+    const data = await res.json();
+    console.log(data);
 }
 
-main()
+main();
