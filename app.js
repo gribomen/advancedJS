7a49536a6b495f6c66ac7a28a6964c0e1a164395
The Great White Rabbit <gribomen@mail.ru>
2026-03-03 16:06:01 +0300

    Выполнил упражнение мой-город получил его через асинхронные функции

--- app.js
+++ app.js
@@ -1,47 +1,56 @@
 'use strict';
 
-// 1 Вариант реализации
-// function myFetch(url){
-//     return new Promise((resolve,reject)=>{
-//     const request = new XMLHttpRequest();
-//         request.open('GET',url);
-//         request.send();
-//         request.addEventListener('load',function(){
-//             try{
-//                 resolve(this.response);
-//             }catch(error){
-//                 reject(new Error('error'));
-//             }
-//         })
-//     });
-// }
+// Мой вариант
+// const promise = new Promise ( (resolve, reject) => {  navigator.geolocation.getCurrentPosition((position) => {
+//         resolve(fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`));
+    
+//     },(error) => {
+//         reject(error);
+    
+//     })
+// });
 
-// 2 Вариант специализации
-function myFetch(url){
-    return new Promise((resolve,reject)=>{
-    const request = new XMLHttpRequest();
-        request.open('GET',url);
-        request.send();
-        request.addEventListener('load',function(){
-            if(this.status > 400){
-                reject(new Error(this.status));
+// promise
+//     .then((response) => {
+//         if(!response.ok){
+//             throw new Error('Error: '+ response.status);
+//         }
+//         return response.json();
+//     })
+//     .then(({city}) => {
+//         console.log(city);
+//     })
+//     .catch(error => console.error(error));
+
+function getMyCoordinates(){
+    return new Promise((resolve, reject)=>{
+        navigator.geolocation.getCurrentPosition(
+            ({ coords }) => {
+                resolve({
+                    latitude: coords.latitude,
+                    longitude: coords.longitude,
+                })
+            }, 
+            (error) => {
+                reject(error);
             }
-            resolve(this.responseText);
-
-        })
-
-        request.addEventListener('error',function(){
-            reject(new Error(this.status));
-        })
-
-        request.addEventListener('timeout',function(){
-            reject(new Error('Timeout'));
-
-        })
-    });
-}
-myFetch('https://dummysjson.com/products/')
-    .then(data => {
-        console.log(data);
+        );
+    
     })
-    .catch(error => console.error('Это ошибка: ' + error));
+}
+
+async function getMyCity(){
+    try{
+        const {latitude, longitude} = await getMyCoordinates();
+        const response = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`);
+        if(!response.ok){
+            throw new Error(response.status);
+        }
+        const {city} = await response.json();
+        console.log(city);
+    }catch(error){
+        console.error(error);
+    }
+}
+
+getMyCity()
