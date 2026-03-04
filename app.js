18ae95a9e0463b65e8d5ed1ab71a7a1625844ad5
The Great White Rabbit <gribomen@mail.ru>
2026-03-04 16:02:56 +0300

    Добавил изменения

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-async function race(promises){
+function race(promises){
     return new Promise((resolve,reject) => {
         for( const promise of promises){
             Promise.resolve(promise).then(resolve,reject);
@@ -11,16 +11,19 @@ async function race(promises){
 }
 
 const res = race([
-    new Promise(resolve => resolve(fetch('https://dummyjson.com/products/1'))),
-    new Promise(resolve => resolve(fetch('https://dummyjson.com/products/2'))),
-    new Promise(resolve => resolve(fetch('https://dummyjson.com/products/3'))),
-    new Promise(resolve => resolve(fetch('https://dummyjson.com/products/4'))),
-    new Promise(resolve => resolve(fetch('https://dummyjson.com/products/5'))),
-    new Promise(resolve => resolve(fetch('https://dummyjson.com/products/6'))),
-    new Promise(resolve => resolve(fetch('https://dummyjson.com/products/7'))),
-    new Promise(resolve => resolve(fetch('https://dummyjson.com/products/8'))),
-    new Promise(resolve => resolve(fetch('https://dummyjson.com/products/9'))),
+    fetch('https://dummyjson.com/products/1'),
+    fetch('https://dummyjson.com/products/2'),
+    fetch('https://dummyjson.com/products/3'),
+    fetch('https://dummyjson.com/products/4'),
+    fetch('https://dummyjson.com/products/5'),
+    fetch('https://dummyjson.com/products/6'),
+    fetch('https://dummyjson.com/products/7'),
+    fetch('https://dummyjson.com/products/8'),
+    fetch('https://dummyjson.com/products/9'),
     new Promise((resolve,reject) => reject('Error'))  
-])
+]);
 
-console.log(res);
+res
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(e => console.log(e));
