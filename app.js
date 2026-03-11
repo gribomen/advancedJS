4dae9718ea4661c46dd1100f716a930947a4a119
The Great White Rabbit <gribomen@mail.ru>
2026-03-12 00:02:05 +0300

    Посмотрел как работает сборщик

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
-import {add,sub,res} from './calc.js';
+import {add,sub} from './calc.js';
 
+const a = 2;
 console.log(add(1,3));
 console.log(sub(1,3));
-console.log(res);
-console.log(1);
+console.log(2);
